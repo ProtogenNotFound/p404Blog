83a05d7d27fd501e273b48dcedeb5d6c4a852ea1
ProtogenNotFound <259433249+ProtogenNotFound@users.noreply.github.com>
2026-03-01 21:51:44 +0100

    meta stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'P404 Blog';
+export const SITE_DESCRIPTION = 'Somewhat professional hornyposting';
 
 /** Base URL with trailing slash (e.g. "/p404Blog/") for building internal links on GH Pages. */
 export function getBaseUrl(): string {
